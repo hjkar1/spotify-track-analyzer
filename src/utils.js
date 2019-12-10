efca7f18c4a4c638ea31ec505830248a1a1d7df2
hjkar1 <hjkar1@outlook.com>
2019-12-10 21:41:00 +0200

    add helper function for defining the key of a track

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,36 @@ export const getAuthHeaderConfig = () => {
 
   return null;
 };
+
+// Return the key of a track based on the pitch class integer.
+
+export const getTrackKey = pitchClass => {
+  switch (pitchClass) {
+    case 0:
+      return 'C';
+    case 1:
+      return 'C♯/D♭';
+    case 2:
+      return 'D';
+    case 3:
+      return 'D♯/E♭';
+    case 4:
+      return 'E';
+    case 5:
+      return 'F';
+    case 6:
+      return 'F♯/G♭';
+    case 7:
+      return 'G';
+    case 8:
+      return 'G♯/A♭';
+    case 9:
+      return 'A';
+    case 10:
+      return 'A♯/B♭';
+    case 11:
+      return 'B';
+    default:
+      return 'The key could not be detected.';
+  }
+};
