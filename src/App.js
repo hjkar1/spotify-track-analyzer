575a7eb89af4268d4c967bb600abf6a2dad51122
hjkar1 <hjkar1@outlook.com>
2019-12-10 21:39:48 +0200

    add Context to App, add route for TrackData component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import { ContextProvider } from './state';
 import TrackSearch from './components/TrackSearch';
+import TrackData from './components/TrackData';
 import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
+    <ContextProvider>
       <Switch>
         <Route exact path="/" component={TrackSearch} />
+        <Route exact path="/track/:trackId" component={TrackData} />
         <Route exact path="/authorize" component={Login} />
       </Switch>
-    </div>
+    </ContextProvider>
   );
 }
 
