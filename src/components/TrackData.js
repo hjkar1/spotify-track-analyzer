e6d1ac8bb0f2ecb6b72def82154d629a4160be84
hjkar1 <hjkar1@outlook.com>
2019-12-11 17:03:52 +0200

    add audio feature data and bar chart, fix error dispatch

--- src/components/TrackData.js
+++ src/components/TrackData.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import { useContextState } from '../state';
 import { getAuthHeaderConfig, getTrackKey } from '../utils';
+import BarChart from './BarChart';
 
 const TrackData = ({
   match: {
     params: { trackId }
   }
 }) => {
-  const [{ trackData, loading, error }, dispatch] = useContextState();
+  const [{ trackData, tracks, loading, error }, dispatch] = useContextState();
 
   useEffect(() => {
     const fetchTrackData = async () => {
@@ -26,8 +27,8 @@ const TrackData = ({
         });
       } catch (error) {
         dispatch({
-          type: 'getTrackDataError',
-          error: error
+          type: 'getTrackDataFail',
+          error: 'Audio features not found.'
         });
       }
     };
@@ -38,18 +39,56 @@ const TrackData = ({
     return <Redirect to="/authorize" />;
   }
 
-  const audioFeatures = (
-    <div>
-      <div>The key of the track: {getTrackKey(trackData.key)}</div>
-      <div>The mode of the track: {trackData.mode === 1 ? 'major' : 'minor'}</div>
-    </div>
-  );
+  const getTrackDuration = durationInMilliseconds => {
+    const durationInSeconds = durationInMilliseconds / 1000;
+    const minutes = Math.round(durationInSeconds / 60);
+    const seconds = Math.round(durationInSeconds % 60);
+    return `${minutes}:${seconds}`;
+  };
+
+  const getAudioFeatures = () => {
+    const chartData = [
+      { x: 'acousticness', y: trackData.acousticness },
+      { x: 'danceability', y: trackData.danceability },
+      { x: 'energy', y: trackData.energy },
+      { x: 'instrumentalness', y: trackData.instrumentalness },
+      { x: 'liveness', y: trackData.liveness },
+      { x: 'speechiness', y: trackData.speechiness },
+      { x: 'valence', y: trackData.valence }
+    ];
+
+    return (
+      <div>
+        <div>Estimated audio features:</div>
+        <div>Duration: {getTrackDuration(trackData.duration_ms)}</div>
+        <div>Key: {getTrackKey(trackData.key)}</div>
+        <div>
+          Mode:
+          {trackData.mode === 1
+            ? 'major'
+            : trackData.mode === 0
+            ? 'minor'
+            : 'Unknown'}
+        </div>
+        <div>Beats per minute: {Math.round(trackData.tempo)}</div>
+        <BarChart data={chartData} />
+      </div>
+    );
+  };
+
+  const track = tracks.find(track => track.id === trackId);
 
   return (
     <Fragment>
-      <h1>Audio features</h1>
-      {loading ? <div>Loading...</div> : audioFeatures}
-      <div>{error}</div>
+      <h1>{track.name}</h1>
+      <div>Artists: {track.artists.map(artist => artist.name).join(', ')}</div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        error
+      ) : trackData ? (
+        getAudioFeatures()
+      ) : null}
     </Fragment>
   );
 };
