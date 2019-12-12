f6b91c3b584a100501ec47d8e739ca89d6a576f2
hjkar1 <hjkar1@outlook.com>
2019-12-12 12:45:44 +0200

    refactor Context usage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import { ContextProvider } from './state';
+import { AppContext } from './state';
+import reducer from './reducer';
 import TrackSearch from './components/TrackSearch';
 import TrackData from './components/TrackData';
 import Login from './components/Login';
 
-function App() {
-  return (
-    <ContextProvider>
-      <Switch>
-        <Route exact path="/" component={TrackSearch} />
-        <Route exact path="/track/:trackId" component={TrackData} />
-        <Route exact path="/authorize" component={Login} />
-      </Switch>
-    </ContextProvider>
-  );
-}
+const initialState = { tracks: [], trackData: null, loading: false, error: '' };
+
+const App = () => (
+  <AppContext.Provider value={useReducer(reducer, initialState)}>
+    <Switch>
+      <Route exact path="/" component={TrackSearch} />
+      <Route exact path="/track/:trackId" component={TrackData} />
+      <Route exact path="/authorize" component={Login} />
+    </Switch>
+  </AppContext.Provider>
+);
 
 export default App;
