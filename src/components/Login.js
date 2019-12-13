32a90031b2fb5469198035441bc7280ef5903454
hjkar1 <hjkar1@outlook.com>
2019-12-13 12:55:29 +0200

    add styling to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { LinkButton, Container } from './UIComponents';
 
 const Login = () => {
   return (
-    <Fragment>
-      <div>Login with your Spotify account</div>
-      <a href="https://accounts.spotify.com/authorize?client_id=80e53ec7106c4712acd07cf1ffeca573&redirect_uri=http:%2F%2Flocalhost:3000&scope=user-read-private%20user-read-email&response_type=token&state=123">
+    <Container>
+      <div style={{ margin: '1rem' }}>Login with your Spotify account</div>
+      <LinkButton href="https://accounts.spotify.com/authorize?client_id=80e53ec7106c4712acd07cf1ffeca573&redirect_uri=http:%2F%2Flocalhost:3000&scope=user-read-private%20user-read-email&response_type=token&state=123">
         Login
-      </a>
-    </Fragment>
+      </LinkButton>
+    </Container>
   );
 };
 
