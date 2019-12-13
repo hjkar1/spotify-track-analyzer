15d4c03068ba27d2981dd62dd9c4e8deb74d8e8e
hjkar1 <hjkar1@outlook.com>
2019-12-13 12:54:57 +0200

    add reusable component styling

---
+++ src/components/UIComponents.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
+
+export const LinkButton = styled.a`
+  background-color: green;
+  border-radius: 4px;
+  color: white;
+  margin: 1rem;
+  padding: 0.5rem;
+  text-decoration: none;
+`;
