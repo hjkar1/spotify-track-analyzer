b69c3c365f87971833fbc8b90e88eebd19436023
hjkar1 <hjkar1@outlook.com>
2019-12-13 12:56:06 +0200

    add styling to TrackSearch component

--- src/components/TrackSearch.js
+++ src/components/TrackSearch.js
@@ -1,8 +1,34 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import { Link, Redirect } from 'react-router-dom';
+import styled from 'styled-components';
 import { useContextState } from '../state/state';
 import { getTrackSearch } from '../state/actions';
+import { Container } from './UIComponents';
+
+const SubmitButton = styled.button`
+  background-color: green;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  margin-left: 1rem;
+  padding: 0.5rem;
+  text-align: center;
+  text-decoration: none;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: black;
+  margin: 0.5rem;
+  text-decoration: none;
+`;
 
 const TrackSearch = ({ location }) => {
   const [search, setSearch] = useState('');
@@ -28,25 +54,31 @@ const TrackSearch = ({ location }) => {
   };
 
   return (
-    <Fragment>
-      <h1>Search tracks</h1>
+    <Container>
+      <h1 style={{ marginBottom: '1.5rem' }}>Search tracks</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} value={search} />
-        <button>Search</button>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Track name"
+          value={search}
+          style={{ padding: '0.5rem' }}
+        />
+        <SubmitButton>Search</SubmitButton>
       </form>
       {loading ? (
-        <div>Loading...</div>
+        <div style={{ margin: '1rem' }}>Loading...</div>
       ) : (
-        <div>
+        <LinkContainer>
           {tracks.map(track => (
-            <Link key={track.id} to={`/track/${track.id}`}>
+            <StyledLink key={track.id} to={`/track/${track.id}`}>
               {track.name}
-            </Link>
+            </StyledLink>
           ))}
-        </div>
+        </LinkContainer>
       )}
-      <div>{error}</div>
-    </Fragment>
+      <div style={{ margin: '1rem' }}>{error}</div>
+    </Container>
   );
 };
 
