1a620bb699adc56935ef41f4cd45d1edcedffccf
hjkar1 <hjkar1@outlook.com>
2019-12-17 15:03:30 +0200

    move Button component to its own file in ui folder

---
+++ src/components/ui/Button.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: green;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  margin-left: 1rem;
+  padding: 0.5rem;
+  text-align: center;
+  text-decoration: none;
+`;
+
+export default Button;
